e0e1a07c6d20e84aba42078b63481728d409015f
Kristoff <finley@kristoff.codes>
2021-09-30 17:38:30 -0600

    Git Ignore added

--- Wumpus_World/Wumpus_World/Program.cs
+++ Wumpus_World/Wumpus_World/Program.cs
@@ -7,7 +7,6 @@ namespace Wumpus_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
