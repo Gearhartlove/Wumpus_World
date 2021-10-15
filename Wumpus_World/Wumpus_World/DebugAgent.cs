6883b72df2c2fc959ef1518cd9b99bc936e6a59a
Gearhartlove <finley@kristoff.codes>
2021-10-15 03:43:06 -0600

    Test agent used for debugging the pathing

---
+++ Wumpus_World/Wumpus_World/DebugAgent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Wumpus_World {
+    public class DebugAgent : Agent{
+        public override void Navigate(Board board) {
+            board.SetAgent(this);
+
+            var start = getCell(board);
+            
+            Console.WriteLine(board);
+            MoveEast();
+            Console.WriteLine(board);
+            MoveNorth();
+            Console.WriteLine(board);
+            MoveWest();
+            Console.WriteLine(board);
+            MoveSouth();
+            Console.WriteLine(board);
+            MoveSouth();
+            Console.WriteLine(board);
+            MoveSouth();
+            Console.WriteLine(board);
+            Console.WriteLine("TRAVELING BACK NOW");
+            TravelPath(start);
+            Console.WriteLine("Starting: x:" + start.getX + " y:" + start.getY);
+            Console.WriteLine("Ending: x:" + getCell(board).getX + " y:" + getCell(board).getY);
+        }
+    }
+}
